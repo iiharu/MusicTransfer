6f6308a5acb4eef9098076cd1a8bf3ad056732e2
iiharu <iiharu@alumni.tus.ac.jp>
2020-04-01 08:15:28 +0900

    Initial Commit

---
+++ MusicTransfer/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  MusicTransfer
+//
+//  Created by Yasuharu Iida on 2020/04/01.
+//  Copyright © 2020 Yasuharu Iida. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
