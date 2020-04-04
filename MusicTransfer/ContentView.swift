c1582bf98181c8138f7b18f555043b64d9101f1c
iiharu <iiharu@alumni.tus.ac.jp>
2020-04-04 15:18:39 +0900

    Update

--- MusicTransfer/ContentView.swift
+++ MusicTransfer/ContentView.swift
@@ -42,31 +42,12 @@ struct ContentView_Previews: PreviewProvider {
 
 class iTunesTransfer {
     
-//    class Track {
-//        // let id: Int
-//        // let trackNumber: Int
-//        // let title: String
-//        // let artist: ITLibArtist
-//        // let album: ITLibAlbum
-//        // let location: URL
-//        let mediaItem: ITLibMediaItem
-//        init(mediaItem: ITLibMediaItem) {
-//            self.mediaItem = mediaItem;
-//        }
-//    }
-//    class Playlist {
-//        // let title: String
-//        // let items: [ITLibMediaItem]
-//        let playlist: ITLibPlaylist
-//        init(playlist: ITLibPlaylist) {
-//            self.playlist = playlist
-//        }
-//    }
-    
     var walkman_music_folder: String = "/Volumes/WALKMAN/MUSIC"
     // apiVersion
     // "1.0": iTunes.app, "1.1": Music.app
     var apiVersion: String = "1.1"
+    
+    var dict: Dictionary<NSNumber, String> = [:]
     
     init() {}
     
@@ -80,20 +61,21 @@ class iTunesTransfer {
         // ITLibrary
         guard let library = try? ITLibrary(apiVersion: apiVersion) else {return} // raise Exception
         // AllSongItems
-        
-        // var allPlaylists = library.allPlaylists
-        // var allMediaItems = library.allMediaItems
-        // var allSongItems = allMediaItems.filter({$0.mediaKind == ITLibMediaItemMediaKind.kindSong})
-        
+                
         // Transfer Songs
         let allSongItems = library.allMediaItems.filter({$0.mediaKind == ITLibMediaItemMediaKind.kindSong})
-        
+
         for item in allSongItems {
             transfer(mediaItem: item)
-            return
         }
         
         // Transfer Playlists
+        let allPlaylist = library.allPlaylists.filter({$0.isMaster != true}).filter({$0.items.count > 0})
+
+        for list in allPlaylist {
+            transfer(playlist: list)
+            return
+        }
         
     }
     
@@ -103,14 +85,14 @@ class iTunesTransfer {
             print("mediaItem is not song")
             return
         }
-
+        
         // Track
         let id = mediaItem.persistentID.intValue // NSValue
         guard let location = mediaItem.location else {
             print("mediaItem's location is nil")
             return
         }
-
+        
         // src
         let src: URL = location
         // replace /Users/iiharu/Music/Music/Media.localized/Music -> walkman_music_folder
@@ -129,69 +111,96 @@ class iTunesTransfer {
                 mediaFolderLocation.appendPathComponent(d, isDirectory: true)
             }
         }
-        var str = src.path
-        if let range = str.range(of: mediaFolderLocation.path) {
-            str.replaceSubrange(range, with: walkman_music_folder)
-        }
-        let dst = URL(fileURLWithPath: str)
         
+        // mediaItemPathから定義してるけど、
+        // 転送先から定義して, walkman_music_folderを消すのがいいかも
+        // mediaItemのとこから転送先を作っていくってなるとそっちの方が恋率良さそう
+        var mediaItemPath:String = src.path
+        if let range = mediaItemPath.range(of: (mediaFolderLocation.path + "/")) {
+            mediaItemPath.replaceSubrange(range, with: "")
+        }
+        // Add Playlist
+        dict[mediaItem.persistentID] = mediaItemPath
+        
+        let dst:URL = URL(fileURLWithPath: walkman_music_folder).appendingPathComponent(mediaItemPath)
+
+        //print(mediaItemPath)
+        //print(dst)
+
+
+        // AlbumArtist
+        //        let album = mediaItem.album
+        //        var albumArtist = album.albumArtist
+        //        if (albumArtist == nil) {
+        //            if (album.isCompilation) {
+        //                albumArtist = "Compilation"
+        //            } else {
+        //                albumArtist = mediaItem.artist?.name
+        //            }
+        //        }
+        //        // AlbumTitle
+        //        let albumTitle = album.title
+        //        // AlbumDiscNumber
+        //        let discNumber = album.discNumber
+        //        // TrackNumber
+        //        let trackNumber = mediaItem.trackNumber
+        //        // Title
+        //        let title = mediaItem.title
+        //        // Extension
+        //        let ext = location.pathExtension
+        //        var dst = title + "." + ext
+        //        if (trackNumber > 0) {
+        //            dst = "\(trackNumber) \(dst)"
+        //            if (discNumber > 0) {
+        //                dst = "\(discNumber)-\(dst)"
+        //            }
+        //        }
+        //        if (albumTitle != nil) {
+        //            dst = albumTitle! + "/" + dst
+        //        }
+        //        if (albumArtist != nil) {
+        //            dst = albumArtist! + "/" + dst
+        //        }
+
         // copy
         // file exsits and dir -> ok
         // file exsits and not dir -> ng
         // file not exsits -> ok
         let parentFolderLocation = dst.deletingLastPathComponent()
-        do {
-            var isDir:ObjCBool = ObjCBool(false)
-            if (FileManager.default.fileExists(atPath: dst.path, isDirectory: &isDir)) {
-                if (!isDir.boolValue) {
-                    // raise Error
-                    print("\(parentFolderLocation.path) is exsists, but it is not directory")
-                    return
+        return
+        let dryrun = true
+        if (!dryrun) {
+            do {
+                var isDir:ObjCBool = ObjCBool(false)
+                if (FileManager.default.fileExists(atPath: dst.path, isDirectory: &isDir)) {
+                    if (!isDir.boolValue) {
+                        // raise Error
+                        print("\(parentFolderLocation.path) is exsists, but it is not directory")
+                        return
+                    }
+                } else {
+                        try FileManager.default.createDirectory(at: parentFolderLocation, withIntermediateDirectories: true, attributes: nil)
+              
                 }
-            } else {
-                try FileManager.default.createDirectory(at: parentFolderLocation, withIntermediateDirectories: true, attributes: nil)
+                try FileManager.default.copyItem(at: src, to: dst)
             }
-            try FileManager.default.copyItem(at: src, to: dst)
-        }
-        catch (let e) {
+            catch (let e) {
                 print(e)
             }
         }
-
-        // AlbumArtist
-//        let album = mediaItem.album
-//        var albumArtist = album.albumArtist
-//        if (albumArtist == nil) {
-//            if (album.isCompilation) {
-//                albumArtist = "Compilation"
-//            } else {
-//                albumArtist = mediaItem.artist?.name
-//            }
-//        }
-//        // AlbumTitle
-//        let albumTitle = album.title
-//        // AlbumDiscNumber
-//        let discNumber = album.discNumber
-//        // TrackNumber
-//        let trackNumber = mediaItem.trackNumber
-//        // Title
-//        let title = mediaItem.title
-//        // Extension
-//        let ext = location.pathExtension
-//        var dst = title + "." + ext
-//        if (trackNumber > 0) {
-//            dst = "\(trackNumber) \(dst)"
-//            if (discNumber > 0) {
-//                dst = "\(discNumber)-\(dst)"
-//            }
-//        }
-//        if (albumTitle != nil) {
-//            dst = albumTitle! + "/" + dst
-//        }
-//        if (albumArtist != nil) {
-//            dst = albumArtist! + "/" + dst
-//        }
-
+    }
+    
+    func transfer(playlist: ITLibPlaylist) {
+        print(playlist.name)
+        let items = playlist.items.map({dict[$0.persistentID]}).filter({$0 != nil})
+//        let count: int = items.reduce(0, {$0 + [$0 != nil ]})
+        let count: Int = items.reduce(0, {$0 + (($1 != nil) ? 1 : 0)})
+        let m38u: URL = URL(fileURLWithPath: walkman_music_folder).appendingPathComponent(playlist.name + ".m38u")
+        // print(items)
+        print(items[0])
+        print(items.count)
+        print(count)
+    }
 
 }
 
